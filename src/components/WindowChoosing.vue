e27f2f682244b6041abcaab93ed0e5fc24cc2f94
costiucigor <gast777@yahoo.com>
2024-05-09 13:22:00 +0300

    changes 6

--- src/components/WindowChoosing.vue
+++ src/components/WindowChoosing.vue
@@ -300,6 +300,13 @@ watch(imageCount, () => {
       <div class="modal-overlay"></div>
       <div class="modal-content"  @scroll="handleScroll">
         <div class="container">
+          <div class="close"
+               @click="closeModal">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M18 6L6 18M6 6L18 18" stroke="#222222" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round"/>
+            </svg>
+          </div>
           <h1 class="title">Оконный калькулятор</h1>
           <div class="img-container">
             <button class="img-button" @click="removeImage">
